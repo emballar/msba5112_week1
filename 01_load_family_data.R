63c5433228edf7a19bca4a1ef08eeff41ec6d30e
emballar <erinmichelle.ballar@gmail.com>
2022-09-28 23:55:14 -0700

    fixed files

--- 01_load_family_data.R
+++
@@ -1,7 +0,0 @@
-
-#load libararies
-library(tidyverse)
-library(haven)
-
-#load and read data 
-nfhs <- read_dta("IAHR52FL.dta")
--- code/01_load_family_data.R
+++ code/01_load_family_data.R
@@ -1,2 +1,6 @@
+#load libararies
 library(tidyverse)
 library(haven)
+
+#load and read data 
+nfhs <- read_dta("IAHR52FL.dta")
